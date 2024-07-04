fa7f5053bcf5f2609eeb3f936b653b0d360b7182
jdyang88 <64808996+jdyang88@users.noreply.github.com>
2024-07-05 07:59:19 +0900

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -454,11 +454,11 @@ if st.button('Run LSTM Model to predict future price'):
     # Display Model Outputs and Accuracy
     if trainScore is not None and train_accuracy is not None:
         st.write(f"Train RMSE: {trainScore:.2f}")
-        st.write(f"Train Predicted Accuracy: {train_accuracy:.2f}%")
+        # st.write(f"Train Predicted Accuracy: {train_accuracy:.2f}%")
 
     if testScore is not None and test_accuracy is not None:
         st.write(f"Test RMSE: {testScore:.2f}")
-        st.write(f"Test Predicted Accuracy: {test_accuracy:.2f}%")
+        # st.write(f"Test Predicted Accuracy: {test_accuracy:.2f}%")
 
     last_price = dataset[-1]
     last_price = np.reshape(last_price, (1, 1, 1))
